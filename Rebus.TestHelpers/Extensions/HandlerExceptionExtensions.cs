63273e4b9c08765148fa3a3e0cd7c37b2d74d497
Martin Humlund Clausen <mhc@umbraco.dk>
2021-09-11 22:44:06 +0200

    Added IEnumerable<HandlerException>.ThrowIfNotEmpty extension method

---
+++ Rebus.TestHelpers/Extensions/HandlerExceptionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rebus.TestHelpers.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class HandlerExceptionExtensions
+    {
+        /// <summary>
+        /// Throws Exception if the collection of <see cref="HandlerException"/> is not empty.
+        /// </summary>
+        /// <param name="exceptions"></param>
+        /// <exception cref="AggregateException">Containing a list of exceptions inside <see cref="HandlerException"/></exception>
+        public static void ThrowIfNotEmpty(this IEnumerable<HandlerException> exceptions)
+        {
+            if (exceptions.Any())
+            {
+                throw new AggregateException(exceptions.Select(ex => ex.Exception));
+            }
+        }
+    }
+}
